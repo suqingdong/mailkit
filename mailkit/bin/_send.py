aefd6c41766361d40f9e46394cd2cf6b33ace105
suqingdong <1078595229@qq.com>
2023-09-13 12:07:49 +0800

    add bin cli

---
+++ mailkit/bin/_send.py
@@ -0,0 +1,69 @@
+import click
+from pydantic import ValidationError
+
+from mailkit.core.smtp import SendEmail
+from mailkit.error import LoginError
+
+
+__epilog__ = click.style('''                
+\n\b
+examples:
+  -t to@mail.com -s "test plain" -b "hello world"
+  -t to@mail.com -s "test html" -b "<h1>hello world</h1>" -c html
+  -t to@mail.com -s "test file as body" -b README.md
+  -t to_1@mail.com -t to_2@mail.com -s "test multiple recivers"
+  -t to@mail.com -s "test display with a fake from_addr -f fake@mail.com
+  -t to@mailcom -s "test cc and bcc" -cc cc@mail.com -bcc bcc@mail.com
+  -t to@mail.com -s "test attachments" -a README.md -a requirements.txt 
+''', fg='yellow')
+
+@click.command(
+    name='send',
+    help=click.style('send email to user(s)', fg='green', bold=True),
+    no_args_is_help=True,
+    epilog=__epilog__,
+)
+@click.option('-f', '--from-addr', help='the display address of the sender, default is the login username')
+@click.option('-t', '--to-addrs', help='the recipient address(es)', multiple=True, required=True)
+@click.option('-s', '--subject', help='the subject of the email', required=True)
+@click.option('-b', '--body', help='the body of the email')
+@click.option('-a', '--attachments', help='the attachments of the email', multiple=True)
+@click.option('-C', '--charset', help='the charset of the email', default='utf-8', show_default=True)
+@click.option('-c', '--content-type', help='the content type of the email, eg.: plain, html', default='plain', show_default=True)
+@click.option('-cc', '--cc', help='the cc address(es)', multiple=True)
+@click.option('-bcc', '--bcc', help='the bcc address(es)', multiple=True)
+@click.pass_obj
+def main(obj, **kwargs):
+    main_kwargs = obj['main_kwargs']
+
+    try:
+        mail = SendEmail(**main_kwargs)
+    except ValidationError as e:
+        click.secho(f'[SmtpConfigError] missing required arguments as follow:', fg='red')
+        print(e)
+
+        if click.confirm('Do you want to complete a configration?'):
+            ssl = main_kwargs.get('use_ssl')
+            host = click.prompt('>>> SMTP host', default=main_kwargs.get('host'))
+            port = click.prompt('>>> SMTP port', default=main_kwargs.get('port'))
+            username = click.prompt('>>> SMTP username', default=main_kwargs.get('username'))
+            password = click.prompt('>>> SMTP password', default=main_kwargs.get('password'), hide_input=True)
+            use_ssl = click.prompt('>>> SMTP use ssl', default=main_kwargs.get('use_ssl'), type=bool)
+            timeout = click.prompt('>>> SMTP timeout', default=main_kwargs.get('timeout'), type=int)
+            mail = SendEmail(host=host, port=port, username=username, password=password, use_ssl=use_ssl, timeout=timeout)
+            if click.confirm('Save your configration?'):
+                _env_file = main_kwargs.get('_env_file')
+                mail.save_config(env_file=_env_file)
+                click.secho(f'Configration saved to: {_env_file}', fg='green')
+        else:
+            exit(1)
+
+    try:
+        mail.login()
+    except LoginError as e:
+        click.secho(f'[LoginError] {e}', fg='red')
+        click.secho('please check your payload:')
+        print(mail.model_dump_json(indent=2))
+        exit(2)
+
+    mail.send(**kwargs)
--- mailkit/bin/main.py
+++ mailkit/bin/main.py
@@ -1,1 +1,44 @@
+from pathlib import Path
+
 import click
+
+from mailkit import version_info
+from mailkit.core import SendEmail
+from ._send import main as send_cli
+
+
+CONTEXT_SETTINGS = dict(help_option_names=['-?', '-h', '--help'])
+
+
+@click.group(
+    name=version_info['prog'],
+    help=click.style(version_info['desc'], italic=True, fg='cyan', bold=True),
+    context_settings=CONTEXT_SETTINGS,
+    no_args_is_help=True,
+)
+@click.version_option(version=version_info['version'],
+                      prog_name=version_info['prog'],
+                      message=click.style('%(prog)s, version %(version)s', bold=True, fg='green'))
+@click.option('-H', '--host', help='SMTP server hostname or IP address')
+@click.option('-P', '--port', help='SMTP server port', type=int)
+@click.option('-u', '--username', help='SMTP server username')
+@click.option('-p', '--password', help='SMTP server password')
+@click.option('-s', '--use-ssl', help='Use SSL connection', type=bool, default=True, show_default=True)
+@click.option('-t', '--timeout', help='SMTP server timeout', type=int, default=10, show_default=True)
+@click.option('-e', '--env-file', help='The environment file to load', type=str, default='~/.mailkit.env', show_default=True)
+@click.pass_context
+def cli(ctx, **kwargs):
+    ctx.ensure_object(dict)
+    ctx.obj['main_kwargs'] = dict(
+        _env_file=kwargs.pop('env_file'),
+        **{k: v for k, v in kwargs.items() if v is not None}
+    )
+
+
+def main():
+    cli.add_command(send_cli)
+    cli()
+
+
+if __name__ == '__main__':
+    main()
